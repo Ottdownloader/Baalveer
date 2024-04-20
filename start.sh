fb5e49e1ecf00da3c2ca5253bd3bc1d5a60931ca
Ottdownloader <167221509+Ottdownloader@users.noreply.github.com>
2024-04-20 11:40:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ellisamovies/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/ottdownloader/Baalveer.git /PiroAutoFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PiroAutoFilterBot
